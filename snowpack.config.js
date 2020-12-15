0bd337c81d8f5ccf7793b6b9250890e43066ffa9
Daniel Beeke (NL) <daniel.beeke@om.org>
2020-12-15 10:52:23 +0100

    cleaning up for snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,18 +1,11 @@
-const globImporter = require('node-sass-glob-importer');
-
 module.exports = {
   "scripts": {
-    "mount:html": "mount html --to /",
     "mount:public": "mount public --to /",
-    "mount:src": "mount src --to /",
     "mount:scss": "mount scss --to /css"
   },
   "plugins": [
     [
       "@snowpack/plugin-sass",
-      {
-        importer: globImporter()
-      }
     ]
   ]
 }
