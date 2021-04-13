4270004842006d9a4cd4a5da43a9879da8e905c1
Daniel Beeke (NL) <daniel.beeke@om.org>
2021-04-13 11:54:19 +0200

    Fixing list proxy test

--- cypress/integration/FieldValues.spec.js
+++ cypress/integration/FieldValues.spec.js
@@ -3,14 +3,14 @@ import { ttl2jsonld } from '../../public/js/vendor/ttl2jsonld'
 import * as JsonLdProcessor from '../../public/js/vendor/jsonld';
 
 describe('wrapWithProxy', function () {
-  // it('should be posible to access group @list like it is a standard array', async function () {
-  //   const response = await fetch('http://localhost:8070/fields.ttl')
-  //   const text = await response.text()
-  //   const jsonLd = ttl2jsonld(text)
-  //   const expanded = await JsonLdProcessor.expand(jsonLd)
-  //   const data = wrapWithProxy(expanded[0], ['http://rdf.danielbeeke.nl/recipe/recipe-dev.ttl#ingredients'], true)
-  //   expect(data['ingredients'][0]['http://rdf.danielbeeke.nl/recipe/recipe-dev.ttl#ingredient'][0]['@id']).to.be.equal('http://dbpedia.org/resource/Tomato')
-  // })
+  it('should be posible to access group @list like it is a standard array', async function () {
+    const response = await fetch('http://localhost:8070/fields.ttl')
+    const text = await response.text()
+    const jsonLd = ttl2jsonld(text)
+    const expanded = await JsonLdProcessor.expand(jsonLd)
+    const data = wrapWithProxy(expanded[0], ['http://rdf.danielbeeke.nl/recipe/recipe-dev.ttl#ingredients'], true)
+    expect(data['ingredients'][0]['http://rdf.danielbeeke.nl/recipe/recipe-dev.ttl#ingredient'][0]['@id']).to.be.equal('http://dbpedia.org/resource/Tomato')
+  })
 
   it('should be posible to access list @list like it is a standard array', async function () {
     const response = await fetch('http://localhost:8070/jane-doe.ttl')
@@ -29,8 +29,7 @@ describe('wrapWithProxy', function () {
       'http://localhost:8070/focalPoint.ttl#y1',
       'http://localhost:8070/focalPoint.ttl#y2',
     ], false, true, 'http://schema.org/image')
-    // expect(imageUrl).to.be.equal(data['image'][0]['@value'])
-    // console.log(data['image'][0]['@value'])
-    console.log(data['height'][0]['@value'])
+    expect(imageUrl).to.be.equal(data['image'][0]['@value'])
+    expect(100).to.be.equal(data['height'][0]['@value'])
   })
 })
