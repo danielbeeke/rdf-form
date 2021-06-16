dcab495b3628dc4bef4c8584333567209358ae5e
Daniel Beeke (NL) <daniel.beeke@om.org>
2021-06-16 17:38:23 +0200

    Fixing registry so it will compile with esbuild for now

--- src/js/core/Registry.ts
+++ src/js/core/Registry.ts
@@ -30,13 +30,41 @@ export class Registry extends EventTarget implements CoreComponent {
   async setupElement (definition, bindings: Array<string>, value = null, itemValues = {}, parentValues = null, render = () => null, parent, index = null, children = []): Promise<ElementInstance> {
     const widget = definition['form:widget']?._ && this.registeredFieldClasses[definition['form:widget']?._] ? definition['form:widget']._ : 'unknown'
     let elementClass = this.fieldClasses.get(widget)
-    
+
     if (!elementClass) {
       const widgetPath = this.registeredFieldClasses[definition['form:widget']?._]
-      await import(`${widgetPath}.js`).then(fieldClass => {
+
+      /**
+       * @see https://github.com/evanw/esbuild/issues/700
+       */
+      const fields = {
+        '../elements/Group': () => import('../elements/Group.js'), 
+        '../elements/Container': () => import('../elements/Container.js'), 
+        '../elements/String': () => import('../elements/String.js'), 
+        '../elements/Number': () => import('../elements/Number.js'), 
+        '../elements/Details': () => import('../elements/Details.js'), 
+        '../elements/UrlImage': () => import('../elements/UrlImage.js'), 
+        '../elements/Wrapper': () => import('../elements/Wrapper.js'), 
+        '../elements/Reference': () => import('../elements/Reference.js'), 
+        '../elements/Dropdown': () => import('../elements/Dropdown.js'), 
+        '../elements/Checkbox': () => import('../elements/Checkbox.js'), 
+        '../elements/Color': () => import('../elements/Color.js'), 
+        '../elements/Mail': () => import('../elements/Mail.js'), 
+        '../elements/Url': () => import('../elements/Url.js'), 
+        '../elements/Date': () => import('../elements/Date.js'), 
+        '../elements/Textarea': () => import('../elements/Textarea.js'), 
+        '../elements/Password': () => import('../elements/Password.js'),
+        '../elements/WYSIWYG': () => import('../elements/WYSIWYG.js'), 
+        '../elements/LanguagePicker': () => import('../elements/LanguagePicker.js'), 
+        '../elements/Unknown': () => import('../elements/Unknown.js'), 
+      }
+
+      await (widgetPath in fields ? fields[widgetPath]() : import(`${widgetPath}.js`)).then(fieldClass => {
         const name = Object.keys(fieldClass)[0]
         this.fieldClasses.set(widget, fieldClass[name])
         elementClass = this.fieldClasses.get(widget)
+      }).catch((exception) => {
+        console.log(exception)
       })
     }
 
