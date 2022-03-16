37a7a6964dd0da1356ac16aefb67a5d474aba78c
Daniel Beeke <mail@danielbeeke.nl>
2022-03-16 14:54:47 +0100

    fixing wysiwyg switch button

--- src/elements/WYSIWYG.ts
+++ src/elements/WYSIWYG.ts
@@ -2,13 +2,16 @@ import { Textarea } from './Textarea'
 import { init } from "../vendor/pell";
 import { html } from 'uhtml/async'
 
+const modes = new Map()
+
 export class WYSIWYG extends Textarea {
 
   protected editor
-  protected state = 'wysiwyg'
 
   item () {
-    
+    const id = this.definition['@id'] + (this.index ?? 0)
+    const mode = modes.get(id) ?? 'wysiwyg'
+
     let textValue = this.value?.['@value'] ?? ''
     if (typeof textValue === 'string') textValue.trim()
 
@@ -28,17 +31,22 @@ export class WYSIWYG extends Textarea {
           {
             icon: '&#9998;',
             title: 'Switch to HTML',
-            result: () => {
-              this.state = 'html'
-              this.render()
+            result: async () => {
+              modes.set(id, 'html')
+              await this.render()
             }
           },
 
         ],
         defaultParagraphSeparator: 'p',
         onChange: async html => {
-          if (!this.value) await this.addItem()
-          this.value['@value'] = html
+          if (html) {
+            if (!this.value) await this.addItem()
+            this.value['@value'] = html  
+          }
+          else {
+            this.removeItem()
+          }
           this.dispatchChange()
         },
       })
@@ -47,9 +55,9 @@ export class WYSIWYG extends Textarea {
       element.content.innerHTML = textValue
     }
 
-    return this.state === 'wysiwyg' ? this.editor : html`
+    return mode === 'wysiwyg' ? this.editor : html`
       <button class="button primary switch-editor" onclick=${async () => {
-        this.state = 'wysiwyg'
+        modes.set(id, 'wysiwyg')
         await this.render()
         this.editor.content.innerHTML = this.value?._
       }}>${this.t`Switch to editor`}</button>
